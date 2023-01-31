7dbb71c2f1ea926af696daf5760e7b057341031d
Tomohiko Takahashi <takahashi@tomohiko.io>
2023-02-01 08:46:21 +0900

    接続を受けられるようになった

--- app/server.go
+++ app/server.go
@@ -6,6 +6,21 @@ import (
 	"os"
 )
 
+func replyResponseconn(conn net.Conn) {
+	str := "Hello, net pkg!"
+	data := []byte(str)
+	_, err := conn.Write(data)
+	if err != nil {
+		fmt.Println("cannot write", err)
+	}
+}
+
+func getRequestString(conn net.Conn, c chan string) {
+	data := make([]byte, 1024)
+	count, _ := conn.Read(data)
+	c <- string(data[:count])
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -15,9 +30,19 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		fmt.Println("Connection Accepted!")
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		for {
+			c := make(chan string)
+			go getRequestString(conn, c)
+			receivedCommand := <-c
+
+			fmt.Println(receivedCommand)
+		}
 	}
 }
